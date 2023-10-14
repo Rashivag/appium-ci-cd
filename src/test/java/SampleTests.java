3b70b4aed7397631d7140e718633b22147e7dc55
Rashi <rashia@Rashis-MacBook-Pro.local>
2023-10-14 22:21:50 +0530

    initial commit

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class SampleTests {
+    @Test
+    public void sampleTest(){
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        System.out.println("Executed");
+    }
+}
